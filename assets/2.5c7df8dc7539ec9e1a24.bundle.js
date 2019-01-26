9e0fbd3fd4f602dbe82417f6dd480932c45f17e4
Hill <hill@kimo.com>
2019-01-26 05:29:14 +0000

    fixed miss assets

---
+++ assets/2.5c7df8dc7539ec9e1a24.bundle.js
@@ -0,0 +1,13 @@
+webpackJsonp([2],{
+
+/***/ 181:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = function() {
+  return new Worker(__webpack_require__.p + "569b2bccf7f5d030e06f.worker.js");
+};
+
+/***/ })
+
+});
+//# sourceMappingURL=2.5c7df8dc7539ec9e1a24.bundle.js.map
